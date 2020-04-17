957f5de86daa92a5080a6ea820f0c8a0bec29f40
Chen Yang <yangcnju@gmail.com>
2020-04-16 20:59:41 -0400

    start implementing saving to file

--- src/renderer/components/ControlArea.vue
+++ src/renderer/components/ControlArea.vue
@@ -2,13 +2,13 @@
   <div id="control-area" class="d-flex flex-column">
     <p><strong>Execute Tasks</strong></p>
     <div class="d-flex flex-row justify-content-between">
-      <button class="btn btn-secondary btn-md">
+      <button class="btn btn-secondary btn-md" @click="saveToFileClicked">
         Save to File
       </button>
-      <buttn class="btn btn-secondary btn-md">
+      <button class="btn btn-secondary btn-md" @click="loadFromFileClicked">
         Load from File
-      </buttn>
-      <button class="btn btn-primary btn-md">
+      </button>
+      <button class="btn btn-primary btn-md" @click="runClicked">
         <span>
           Run
         </span>
@@ -18,8 +18,70 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
+const { dialog } = require('electron').remote
+const fs = require('fs')
+
 export default {
-  name: 'control-area'
+  name: 'control-area',
+  computed: {
+    ...mapState('spinsys', ['euler', 'spins', 'interactions']),
+    ...mapState('pulseseq', [
+      'increment', 'emrs', 'sections', 'sequence'
+    ]),
+    ...mapState('SimSettings', [
+      'sample', 'simulation', 'hardware'
+    ])
+  },
+  methods: {
+    prepareSpinsysOutput () {
+      return '{}'
+    },
+    preparePulseseqOutput () {
+      return '{}'
+    },
+    prepareSettingsOutput () {
+      return '{}'
+    },
+    prepareOutput () {
+      const spinsysStr = this.prepareSpinsysOutput()
+      const pulseseqStr = this.preparePulseseqOutput()
+      const settingsStr = this.prepareSettingsOutput()
+      let result = {
+        spinsys: spinsysStr,
+        pulseseq: pulseseqStr,
+        settings: settingsStr
+      }
+      return JSON.stringify(result, null, 2)
+    },
+    prepareInput () {
+
+    },
+    saveToFileClicked () {
+      let content = this.prepareOutput()
+      dialog.showSaveDialog({}
+      ).then(result => {
+        let filename = result.filePath
+        if (filename === undefined) {
+          // nothing created
+          return
+        }
+        fs.writeFile(filename, content, (err) => {
+          if (err) {
+            console.log('an error ocurred with file creation ' + err.message)
+          }
+        })
+      }).catch(err => {
+        alert(err)
+      })
+    },
+    loadFromFileClicked () {
+
+    },
+    runClicked () {
+
+    }
+  }
 }
 </script>
 
